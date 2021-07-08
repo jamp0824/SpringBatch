92c954c7460d2150151d2fce87aefebac6183215
june <loneyjune@yahoo.ca>
2021-07-09 00:42:12 +0900

    reinitializing about springBatch commit

---
+++ src/main/java/com/javainuse/springboot_batch/App.java
@@ -0,0 +1,13 @@
+package com.javainuse.springboot_batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
